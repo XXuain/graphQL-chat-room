c87beea57439df3e8c218e3eab9b21b4433d0360
yellowHuang <hlady1115@gmail.com>
2021-11-28 21:44:27 +0800

    update: use react hooks to refeter com

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
+import React, { useState } from 'react';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import {
+  messagesQuery,
+  addMessageMutation,
+  messageAddedSubscription,
+} from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
-class Chat extends Component {
-  state = { messages: [] };
-  subscription = null;
+const Chat = ({ user }) => {
+  const [messages, setMessages] = useState([]);
+  useQuery(messagesQuery, {
+    onCompleted: ({ messages }) => setMessages(messages),
+  });
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      setMessages((pre) => [...pre, subscriptionData.data.MessageAdded]);
+    },
+  });
 
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({ messages });
-    this.subscription = onMessageAdded((message) => {
-      this.setState({ messages: this.state.messages.concat(message) });
-    });
-  }
+  const [addMessage] = useMutation(addMessageMutation);
 
-  componentWillUnmount() {
-    this.subscription && this.subscription.unsubscribe();
-  }
+  const handleSend = async (text) => {
+    await addMessage({ variables: { input: { text } } });
+  };
 
-  async handleSend(text) {
-    await addMessage(text);
-  }
-
-  render() {
-    const { user } = this.props;
-    const { messages } = this.state;
-    return (
+  return (
+    <>
       <section className="section">
         <div className="container">
           <h1 className="title">Chatting as {user}</h1>
           <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
+          <MessageInput onSend={handleSend} />
         </div>
       </section>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default Chat;
