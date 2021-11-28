8eb9f803af52232d2acb127b6785b716dfdf450a
yellowHuang <hlady1115@gmail.com>
2021-11-28 19:48:41 +0800

    add: pubSub push message to client

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,8 @@
+const { PubSub } = require('graphql-subscriptions');
 const db = require('./db');
+
+const MESSAGE_ADDED = 'MESSAGE_ADDED';
+const pubSub = new PubSub();
 
 function requireAuth(userId) {
   if (!userId) {
@@ -7,18 +11,26 @@ function requireAuth(userId) {
 }
 
 const Query = {
-  messages: (_root, _args, {userId}) => {
+  messages: (_root, _args, { userId }) => {
     requireAuth(userId);
     return db.messages.list();
-  }
-}
+  },
+};
 
 const Mutation = {
-  addMessage: (_root, {input}, {userId}) => {
+  addMessage: (_root, { input }, { userId }) => {
     requireAuth(userId);
-    const messageId = db.messages.create({from: userId, text: input.text});
-    return db.messages.get(messageId);
-  }
-}
+    const messageId = db.messages.create({ from: userId, text: input.text });
+    const messages = db.messages.get(messageId);
+    pubSub.publish(MESSAGE_ADDED, { MessageAdded: messages });
+    return messages;
+  },
+};
 
-module.exports = { Query, Mutation };
+const Subscription = {
+  MessageAdded: {
+    subscribe: () => pubSub.asyncIterator(MESSAGE_ADDED),
+  },
+};
+
+module.exports = { Query, Mutation, Subscription };
